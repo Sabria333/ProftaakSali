4fff870b2d2710a560678d3d957dd7c5d4513619
Sabria <75360585+Sabria333@users.noreply.github.com>
2023-05-23 10:43:22 +0200

    nieuw
    project files

---
+++ about.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title>About</title>
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+<header>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="shop.php">Shop</a></li>
+          <li><a href="about.php">About Us</a></li>
+          <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
+  <section class="about-us">
+    <div class="about">
+      <div class="text">
+        <h2>Ons merk</h2>
+        <h5>Sali since 2023 <span>Liridon Berisha & Sabria El Khadir</span></h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Expedita natus ad sed harum itaque ullam enim quas, veniam accusantium,
+             quia animi id eos adipisci iusto molestias asperiores explicabo cum vero 
+             atque amet corporis! Soluta illum facere consequuntur magni. Ullam dolorem 
+             repudiandae cumque voluptate consequatur consectetur, eos provident necessitatibus reiciendis corrupti!</p>
+        <div class="data">
+        <a href="contact.php" class="hire">Contact opnemen</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Bijen animatie js -->
+  <div class="container">
+      <div class="wings"></div>
+      <div class="bee">
+        <div class="shine"></div>
+      </div>
+    </div>
+
+    <div class="wrapper">
+    <h1>Het Team</h1>
+    <div class="our_team">
+        <div class="team_member">
+          <div class="member_img">
+             <img src="member_1.png" alt="our_team">
+            <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Liridon Berisha</h3>
+          <span>CEO</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione perspiciatis, error deleniti quaerat beatae doloribus incidunt excepturi. Fugit deleniti accusantium neque hic quidem voluptatibus cumque.</p>
+        </div>
+        <div class="team_member">
+           <div class="member_img">
+             <img src="member_2.png" alt="our_team">
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Sabria El Khadir</h3>
+          <span>CEO</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+             Dolores maiores temporibus, architecto optio asperiores mollitia 
+             pariatur error, quaerat voluptatibus minima eos quo nostrum, maxime necessitatibus.</p>
+      </div>
+     
+
+</body>
+</html>
